9cbf4ebbe97712e995c635a83b36348fc2013faf
Petar Patev <petarpatev@abv.bg>
2024-05-12 10:38:53 +0300

    create universal request function

---
+++ src/api.js
@@ -0,0 +1,52 @@
+const baseUrl = 'http://localhost:3030';
+
+async function request(method, url, data) {
+
+    const options = {
+        method,
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    if (data != undefined) {
+        options.body = JSON.stringify(data);
+    }
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+        const token = user.accessToken;
+        options.headers['X-Authorization'] = token;
+    }
+
+    try {
+        const response = await fetch(baseUrl + url, options);
+
+        if (response.ok != true) {
+            if (response.status == 403) {
+                localStorage.removeItem('user');
+            }
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status == 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export {
+    get,
+    post,
+    put,
+    del as delete
+}
