0ae505aa255dc84ef475bf6165e3dde3137ab7b9
Petar Patev <petarpatev@abv.bg>
2024-05-13 09:27:40 +0300

    Add navigation between pages

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
-import * as api from './user.js';
-window.api = api;
+const homePage = document.getElementById('home');
+const registerPage = document.getElementById('register');
+const loginPage = document.getElementById('login');
+const catalogPage = document.getElementById('dashboard-holder');
+const detailsPage = document.getElementById('details');
+const createPage = document.getElementById('create');
+
+const pageWrapper = document.getElementById('page-wrapper');
+
+const allPages = document.getElementById('page-container');
+allPages.remove();
+
+const pages = {
+    '/': homePage,
+    '/register': registerPage,
+    '/login': loginPage,
+    '/catalog': catalogPage,
+    '/details': detailsPage,
+    '/create': createPage
+}
+
+function showPage(path) {
+    const page = pages[path];
+    pageWrapper.replaceChildren(page);
+}
+window.showPage = showPage;
