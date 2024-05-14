86c4fe2f4728e4562a5f8112546bd4ea4ca86369
Petar Patev <petarpatev@abv.bg>
2024-05-14 10:03:43 +0300

    Add create idea logic

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
-import { get } from './api.js';
+import { get, post } from './api.js';
 
 const endPoints = {
     'getAllIdeas': '/data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc',
+    'createIdea': '/data/ideas',
 }
 
 export async function getAllIdeas() {
     return get(endPoints.getAllIdeas);
+}
+
+export async function createIdea(ideaData) {
+    return post(endPoints.createIdea, ideaData);
 }
--- src/pages/create.js
+++ src/pages/create.js
@@ -1,5 +1,25 @@
+import { createIdea } from "../data.js";
+
 const page = document.getElementById('create');
 
+let ctx;
+
 export function showCreatePage(context) {
+    ctx = context;
     context.showPage(page);
+}
+
+const createIdeaForm = page.querySelector('form');
+createIdeaForm.addEventListener('submit', onSubmit);
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get('title');
+    const description = formData.get('description');
+    const img = formData.get('imageURL');
+
+    await createIdea({ title, description, img });
+    ctx.updateNav();
+    ctx.navigateTo('/catalog');
 }
